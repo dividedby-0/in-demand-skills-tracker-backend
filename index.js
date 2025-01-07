3664f981bf0c3feef0f6a2b37678e9b3962b2ec7
Alessandro <a.ghio86@gmail.com>
2025-01-07 16:14:08 +0100

    refactor: use env.MONGO_URL instead of local db

--- index.js
+++ index.js
@@ -8,16 +8,16 @@ const setRoutes = require("./routes/setRoutes");
 const app = express();
 
 // Connect to MongoDB
-mongoose.connect("mongodb://0.0.0.0:27017/in-demand-skills-tracker", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 });
 
 // Handle MongoDB connection events
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 db.once("open", () => {
-  console.log("Connected to MongoDB");
+    console.log("Connected to MongoDB");
 });
 
 // Middleware
@@ -30,11 +30,11 @@ app.use("/api/custom-set", authMiddleware.verifyToken, setRoutes);
 
 // Error handling middleware
 app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({ message: "Server error" });
+    console.error(err.stack);
+    res.status(500).json({message: "Server error"});
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
