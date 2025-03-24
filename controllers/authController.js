7efb43d330d104f7f7b03577b41699e7f0bf6997
Alessandro <a.ghio86@gmail.com>
2025-03-24 16:11:55 +0100

    feat: improved user register validations

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,41 +4,74 @@ const User = require("../models/user");
 const CustomError = require("../errors");
 
 exports.register = async (req, res, next) => {
-  try {
-    const { username, password } = req.body;
-    const hashedPassword = await bcrypt.hash(password, 10);
+    try {
+        const {username, email, password} = req.body;
 
-    const user = new User({ username, password: hashedPassword });
-    await user.save();
+        if (username.length < 5 || username.length > 20) {
+            throw new CustomError("Username must be between 5 and 20 characters", 400);
+        }
+        if (!/^[a-zA-Z0-9_-]+$/.test(username)) {
+            throw new CustomError("Username can only contain letters, numbers, underscores, and hyphens", 400);
+        }
 
-    const token = jwt.sign({ userId: user._id }, process.env.SECRET_KEY, { expiresIn: '1h' });
+        if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+            throw new CustomError("Invalid email format", 400);
+        }
 
-    res.json({ token });
-  } catch (error) {
-    next(new CustomError('Failed to register user', 500));
-  }
+        if (password.length < 8) {
+            throw new CustomError("Password must be at least 8 characters long", 400);
+        }
+
+        if (!/[A-Z]/.test(password)) {
+            throw new CustomError("Password must contain at least one uppercase letter", 400);
+        }
+
+        if (!/[a-z]/.test(password)) {
+            throw new CustomError("Password must contain at least one lowercase letter", 400);
+        }
+
+        if (!/[0-9]/.test(password)) {
+            throw new CustomError("Password must contain at least one number", 400);
+        }
+
+        const existingUser = await User.findOne({email});
+        if (existingUser) {
+            throw new CustomError("Email already registered", 400);
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const user = new User({username, email, password: hashedPassword});
+        await user.save();
+
+        const token = jwt.sign({userId: user._id}, process.env.SECRET_KEY, {expiresIn: '1h'});
+
+        res.json({token});
+    } catch (error) {
+        next(error);
+    }
 };
 
 exports.login = async (req, res, next) => {
-  try {
-    const { username, password } = req.body;
+    try {
+        const {username, password} = req.body;
 
-    const user = await User.findOne({ username });
+        const user = await User.findOne({username});
 
-    if (!user) {
-      throw new CustomError('Invalid credentials', 401);
+        if (!user) {
+            throw new CustomError('Invalid credentials', 401);
+        }
+
+        const isValidPassword = await bcrypt.compare(password, user.password);
+
+        if (!isValidPassword) {
+            throw new CustomError('Invalid credentials', 401);
+        }
+
+        const token = jwt.sign({userId: user._id}, process.env.SECRET_KEY, {expiresIn: '1h'});
+
+        res.json({token});
+    } catch (error) {
+        next(error);
     }
-
-    const isValidPassword = await bcrypt.compare(password, user.password);
-
-    if (!isValidPassword) {
-      throw new CustomError('Invalid credentials', 401);
-    }
-
-    const token = jwt.sign({ userId: user._id }, process.env.SECRET_KEY, { expiresIn: '1h' });
-
-    res.json({ token });
-  } catch (error) {
-    next(error);
-  }
 };
